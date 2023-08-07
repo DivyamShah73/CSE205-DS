4d39637019bd2bcf120cc72cd434dc1d2b1461c8
Divyam Shah <divyamshah04@gmail.com>
2023-08-07 13:52:35 +0530

    Smallest Even Multiple

--- Assignments/practice .cpp
+++ Assignments/practice .cpp
@@ -1,1 +1,11 @@
-test
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }
+        else{
+            return n*2;
+        }
+    }
+};
