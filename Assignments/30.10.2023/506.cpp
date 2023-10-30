1e729b5e5b003fc954ed10364682a0e1b3b0cc26
Divyam Shah <divyamshah04@gmail.com>
2023-10-30 14:22:51 +0530

    Corrected Code

--- Assignments/30.10.2023/506.cpp
+++ Assignments/30.10.2023/506.cpp
@@ -1,28 +1,37 @@
-public class Solution {
-    public string[] FindRelativeRanks(int[] score) {
-        string[] res = new string[score.Length];
-        PriorityQueue<int, int> pq = new();
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        priority_queue<pair<int,int>> maxHeap;
 
-        for(int i=0; i<score.Length; i++){
-            pq.Enqueue(i, -score[i]);
-        }
-
-        if(pq.Count > 0){
-            res[pq.Dequeue()] = "Gold Medal";
+        for(int i=0;i<n;i++) {
+            maxHeap.push({score[i],i});
         }
 
-        if(pq.Count > 0){
-            res[pq.Dequeue()] = "Silver Medal";
-        }
-        
-        if(pq.Count > 0){
-            res[pq.Dequeue()] = "Bronze Medal";
-        }
-        
-        for(int i=4; pq.Count > 0; i++){
-            res[pq.Dequeue()] = $"{i}";
+        vector<string> res(n);
+
+        for(int i=0;i<n;i++) {
+            auto it = maxHeap.top();
+            int currVal = it.first;
+            int index = it.second;
+            maxHeap.pop();
+
+            if(i == 0) {
+                res[index] = "Gold Medal";
+            }
+            else if(i == 1) {
+                res[index] = "Silver Medal";
+            }
+            else if(i == 2) {
+                res[index] = "Bronze Medal";
+            }
+
+            else {
+                string temp = to_string(i+1);
+                res[index] = temp;
+            }
         }
 
         return res;
     }
-}
+};
