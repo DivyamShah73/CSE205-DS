a592d984bc60b590a04f451d592d9b6bad05e7a7
Divyam Shah <divyamshah04@gmail.com>
2023-11-26 23:21:55 +0530

    updated

---
+++ Assignments/30.10.2023/1046.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>> pq;
+        for (auto it : stones) {
+            pq.push(it);
+        }
+
+        while (!pq.empty()) {
+            int a = pq.top();
+            pq.pop();
+
+            if (pq.empty()) {
+                return a;
+            }
+            else {
+                int b = pq.top();
+                pq.pop();
+
+                pq.push(abs(a - b));
+            }
+        }
+
+        return 0;
+    }
+};
+
